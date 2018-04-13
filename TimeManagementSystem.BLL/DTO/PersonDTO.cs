a38c82a09d55a37e08ec664492a75a5eb155c17b
eu-volchenko <eu.volchenko.kh@gmail.com>
2018-04-12 19:31:17 -0900

    I don't understand what's happen

---
+++ TimeManagementSystem.BLL/DTO/PersonDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManagementSystem.BLL.DTO
+{
+    class PersonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TimeManagementSystem.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeManagementSystem.DAL.Entities;
+using TimeManagementSystem.DAL.Interfaces;
+
+namespace TimeManagementSystem.DAL.Repositories
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        public IRepository<Activity> Activities
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<ActivitiesInProject> ActivityInProject
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Person> Persons
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Project> Projects
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Report> Reports
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Risk> Risks
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Role> Roles
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Status> Statutes
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Entities.Task> Tasks
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IRepository<Teammate> Teammates
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
